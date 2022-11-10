3493434ba5b57cde5cb6bb12739acdda0f1522a5
Camilla Kolby Thomsen <camillakthomsen@hotmail.com>
2022-11-10 11:11:16 +0100

    Add project files.

---
+++ Blodtryksmaaler_RPi_4/Program.cs
@@ -0,0 +1,30 @@
+﻿using RaspberryPiNetCore.ADC;
+using RaspberryPiNetCore.JoySticks;
+using RaspberryPiNetCore.LCD;
+using RaspberryPiNetCore.TWIST;
+using System;
+using LogicLayer_RPi;
+using DataLayer_RPi;
+using System.Threading;
+
+namespace Raspberry_Pi_Dot_Net_Core_Console_Application3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            measurementcontroleRPi test = new measurementcontroleRPi();
+            while(true)
+            {
+                foreach (double s in test.GetBPData())
+                {
+                    Console.WriteLine(Convert.ToString(s));
+                }
+
+            }
+               
+        }
+    }
+}
---
+++ LogicLayer_RPi/measurementcontroleRPi.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer_RPi;
+using System.Threading;
+
+namespace LogicLayer_RPi
+{
+    public class measurementcontroleRPi
+    {
+        receivesBloodPressureMeasurement receivesBloodPressure;
+        List<double> voltages;
+        public measurementcontroleRPi()
+        {
+            receivesBloodPressure = new receivesBloodPressureMeasurement();
+        }
+        public List<double> GetBPData()
+        {
+            voltages = new List<double>();
+
+            for(int i = 0; i < 200; i++)
+            {
+                double voltage = receivesBloodPressure.MeasureBP();
+                voltages.Add(voltage);
+                Thread.Sleep(5);
+
+            }
+            
+
+            return voltages;
+        }
+    }
+}
