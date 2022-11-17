528e371d55ae45e830a549a980d24cdf224c9e3b
Camilla Kolby Thomsen <camillakthomsen@hotmail.com>
2022-11-17 12:02:01 +0100

    kode til calculations

--- LogicLayer_RPi/BPCalculator.cs
+++ LogicLayer_RPi/BPCalculator.cs
@@ -9,21 +9,73 @@ namespace LogicLayer_RPi
 {
     public class BPCalculator : IBPCalculator
     {
-        public double getPuls(List<double> measurement)
+        public double systole { get; private set; }
+        public double diastole { get; private set; }
+        public double middel { get; private set; }
+        public int puls { get; private set; }
+        public List<double> measurement;
+
+        public void calcAverage(List<double>measurement)
         {
-            return 0;
+            double totalBP = measurement.Sum();
+            int bpDataPoints = measurement.Count();
+            double averageBP = totalBP / bpDataPoints;
+
+            getSysBP(bpDataPoints, averageBP);
+            getDiaBP(bpDataPoints, averageBP);
+
+            middel = (((systole - diastole) * 1 / 3) + diastole);
+
         }
-        public double getMiddleBP(List<double> measurement)
+        
+        public double getSysBP(int bpDataPoints, double averageBP)
         {
-            return 0;
+            double highLimit = averageBP*1.03;
+            double highPeakTotal = 0;
+            int highPeakCounter = 0;
+
+            for (int i=0;i<bpDataPoints; i++)
+            {
+                if(measurement[i]>highLimit&&measurement[i]>measurement[i-1]&&measurement[i]>measurement[i+1])
+                {
+                    highPeakTotal += measurement[i];
+                    highPeakCounter++;
+                }
+            }
+
+            double sys = highPeakTotal / highPeakCounter;
+            systole = sys;
+            puls = highPeakCounter;
+            return systole;
         }
-        public double getSysBP(List<double> measurement)
+        public double getDiaBP(int bpDataPoints, double averageBP)
         {
-            return 0;
+            double lowLimit = averageBP * 0.97;
+            double lowPeakTotal = 0;
+            int lowPeakCounter = 0;
+           
+
+            for (int i = 0; i < bpDataPoints; i++)
+            {
+                if (measurement[i] < lowLimit && measurement[i] < measurement[i - 1] && measurement[i] < measurement[i + 1])
+                {
+                    lowPeakTotal += measurement[i];
+                    lowPeakCounter++;
+                }
+            }
+
+            double dia = lowPeakTotal / lowPeakCounter;
+            diastole = dia;
+            return systole;
         }
-        public double getDiaBP(List<double> measurement)
+        public double getMiddleBP()
         {
-            return 0;
+            return middel;
+        }
+        public int getPuls()
+        {
+
+            return puls;
         }
     }
 }
